26e856a886fd92de9e8d75109eb46d83409c5544
Rana-Mostafa <rana.mustafa.9924@gmail.com>
2025-09-07 04:43:47 +0300

    Add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "#{@user.username} was successfully updated."
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    flash[:notice] = "#{@user.username} was successfully deleted and signed out."
+    redirect_to root_path
+  end
+
+  private
+    def set_user
+      @user = User.find(params.expect(:id))
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
+
