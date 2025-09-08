6752ead9ef7b6993f9e2300b52f0aaf3a244f629
Rana-Mostafa <rana.mustafa.9924@gmail.com>
2025-09-08 10:39:26 +0300

    Add admin permissions and its validations

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "admin", email: "admin@example.com", password: "password", admin: true)
+  end
+
   test "get new category form and create category" do
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
     assert_difference "Category.count", 1 do
@@ -14,6 +19,7 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
   end
 
   test "get new category form and reject invalid category submission" do
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
     assert_no_difference "Category.count" do
